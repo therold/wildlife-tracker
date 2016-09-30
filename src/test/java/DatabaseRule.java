68f0fb008e2d17df2fdfaccd091473fa4a6a70ce
Timothy Herold <heroldt@gmail.com>
2016-09-29 19:26:49 -0700

    Add database connection and table-clearing rules to DatabaseRule.java.

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,23 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteAnimalsQuery = "DELETE FROM animals *;";
+      String deleteRangersQuery = "DELETE FROM rangers *;";
+      String deleteLocationsQuery = "DELETE FROM locations *;";
+      String deleteSightingsQuery = "DELETE FROM sightings *;";
+      con.createQuery(deleteAnimalsQuery).executeUpdate();
+      con.createQuery(deleteRangersQuery).executeUpdate();
+      con.createQuery(deleteLocationsQuery).executeUpdate();
+      con.createQuery(deleteSightingsQuery).executeUpdate();
+    }
+  }
