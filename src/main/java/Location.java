73277029dee3afd2c64eb78f1916653804da9f41
Timothy Herold <heroldt@gmail.com>
2016-09-30 12:37:36 -0700

    Verify that xCoord instatiates properly and can be set properly for Location.

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,11 +1,13 @@
 public class Location {
   private int id;
   private String name;
+  private double xCoord;
 
   public Location(String name, double xCoord, double yCoord) {
     if(DatabaseManagement.nameValidation(name)) {
       this.name = name;
     }
+    this.xCoord = xCoord;
   }
 
   public int getId() {
@@ -14,6 +16,20 @@ public class Location {
 
   public String getName() {
     return this.name;
+  }
+
+  public void setName(String name) {
+    if(DatabaseManagement.nameValidation(name)) {
+      this.name = name;
+    }
+  }
+
+  public double getXCoord() {
+    return this.xCoord;
+  }
+
+  public void setXCoord(double xCoord) {
+    this.xCoord = xCoord;
   }
 
 }
--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -32,5 +32,33 @@ public class LocationTest {
     Location testLocation = new Location("", 1.525, -2.311);
   }
 
+  @Test
+  public void setUserName_setsANewName_NewName() {
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    testLocation.setName("NewName");
+    assertEquals("NewName", testLocation.getName());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setUserName_cannotSetEmptyName_IllegalArgumentException() {
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    testLocation.setName("");
+  }
+
+
+  // xCoord
+  @Test
+  public void ranger_instantiatesWithXCoord_1_525() {
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    assertEquals(1.525, testLocation.getXCoord(), 0);
+  }
+
+  @Test
+  public void setXCoord_setsANewXCoord_3_885() {
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    testLocation.setXCoord(3.885);
+    assertEquals(3.885, testLocation.getXCoord(), 0);
+  }
+
 
 }
