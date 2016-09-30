561417e1e4a649ebf0c5c3fb190d0fbfe78259fb
Timothy Herold <heroldt@gmail.com>
2016-09-30 12:28:54 -0700

    Verify that Name instantiates propery on Location.

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,12 +1,19 @@
 public class Location {
   private int id;
+  private String name;
 
   public Location(String name, double xCoord, double yCoord) {
-
+    if(DatabaseManagement.nameValidation(name)) {
+      this.name = name;
+    }
   }
 
   public int getId() {
     return this.id;
+  }
+
+  public String getName() {
+    return this.name;
   }
 
 }
--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -20,4 +20,17 @@ public class LocationTest {
     assertEquals(0, testLocation.getId());
   }
 
+  // Name
+  @Test
+  public void ranger_instantiatesWithName_User() {
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    assertEquals("Near bridge", testLocation.getName());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ranger_cannotInstantiateEmptyUserName_IllegalArgumentException() {
+    Location testLocation = new Location("", 1.525, -2.311);
+  }
+
+
 }
