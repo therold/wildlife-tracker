c873e0480909bc21b415e75ca015b2301b1bb7c6
Timothy Herold <heroldt@gmail.com>
2016-09-29 20:45:52 -0700

    Add DatabaseManagement interface to Animal. Used stubs for now.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,4 +1,4 @@
-public class Animal {
+public class Animal implements DatabaseManagement {
   private String name;
 
   public Animal(String name) {
@@ -11,6 +11,24 @@ public class Animal {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public void save() {
+    //TODO
+  }
+
+  public void update() {
+    //TODO
+  }
+
+  public void delete() {
+    //TODO
+  }
+
+  @Override
+  public boolean equals(Object otherObject) {
+    //TODO
+    return false;
   }
 
 }
